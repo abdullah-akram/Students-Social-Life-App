165a35a9661ce1d7549a9d4d21741c864f5a219b
abdullah-akram <mabdullahakram2000@gmail.com>
2023-03-25 16:59:14 +0500

    Full Design Changes + bugs resolved

--- app/src/main/java/com/smd/wssl_app/SignUpFragment.java
+++ app/src/main/java/com/smd/wssl_app/SignUpFragment.java
@@ -417,7 +417,7 @@ FirebaseAuth mauth;FirebaseFirestore db;
                 // The SMS verification code has been sent to the provided phone number, we
                 // now need to ask the user to enter the code and then construct a credential
                 // by combining the code with a verification ID.
-                Log.d("TAG", "---onCodeSent:" + verificationId);
+                Log.d("TAAG", "---onCodeSent:" + verificationId);
 
                 // Save verification ID and resending token so we can use them later
 //                mVerificationId = verificationId;
